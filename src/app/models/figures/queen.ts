da7058ff3f3a5a84cf5687ea6e8689fea50c8b38
mummick <mummick6@gmail.com>
2021-06-26 14:56:23 +0300

    feat: add Queen

---
+++ src/app/models/figures/queen.ts
@@ -0,0 +1,45 @@
+import { CellCoord } from '../cell-coord';
+import { ChessColor } from '../chess-color';
+import { COMMON } from '../common';
+import { Figure } from '../figure';
+import { FigureType } from '../figure-type';
+import { IField } from '../ifield';
+import { Move } from '../move';
+import { Moves } from '../moves';
+import { Vector } from '../vector';
+
+export class Queen extends Figure {
+  constructor(color: ChessColor) {
+    super(FigureType.queen, color);
+  }
+  getMoves(position: CellCoord, field: IField): Moves {
+    const result = new Moves();
+    for (let vector of COMMON.DIAGONAL_MOVES) {
+      let resultPosition = vector.resultPosition(position);
+      let currentVector = vector.copy();
+      while (resultPosition.isCorrect() && (field.isFreeCell(resultPosition) || field.getFigure(resultPosition)?.color !== this.color)) {
+        result.add(new Move(position, currentVector));
+        if (!field.isFreeCell(resultPosition)) {
+          currentVector = new Vector(COMMON.BOARD_SIZE, COMMON.BOARD_SIZE); //exit from While
+        } else {
+          currentVector = currentVector.sum(vector);
+        }
+        resultPosition = currentVector.resultPosition(position);
+      }
+    }
+    for (let vector of COMMON.HV_MOVES) {
+      let resultPosition = vector.resultPosition(position);
+      let currentVector = vector.copy();
+      while (resultPosition.isCorrect() && (field.isFreeCell(resultPosition) || field.getFigure(resultPosition)?.color !== this.color)) {
+        result.add(new Move(position, currentVector));
+        if (!field.isFreeCell(resultPosition)) {
+          currentVector = new Vector(COMMON.BOARD_SIZE, COMMON.BOARD_SIZE); //exit from While
+        } else {
+          currentVector = currentVector.sum(vector);
+        }
+        resultPosition = currentVector.resultPosition(position);
+      }
+    }
+    return result;
+  }
+}
